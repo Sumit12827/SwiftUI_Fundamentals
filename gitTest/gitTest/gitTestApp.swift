10f0fa108a77226a777646dbf4c18e518e5dc412
Sumit12827 <sumitdowney@gmail.com>
2025-12-15 11:09:27 +0530

    new file

---
+++ gitTest/gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gittestApp.swift
+//  gittest
+//
+//  Created by Sumit Downey on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct gittestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
